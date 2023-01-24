c271d234d23c5f1872bd2a9e58fbb4b9be1a8ebf
Brando Vidal <yisusdracon@gmail.com>
2023-01-24 12:09:16 -0500

    ⚒  change ts-node-dev to ts-node and remove prisma-zod-types
    Signed-off-by: Brando Vidal <yisusdracon@gmail.com>

--- src/components/post/schema.ts
+++ src/components/post/schema.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod'
 
 export const postSchema = z.object({
-  authorId: z.string({ required_error: 'Author ID is required', invalid_type_error: 'Author ID must be a string' }).uuid({ message: 'Author ID must be a valid UUID' }),
+  authorId: z.string({ required_error: 'Author ID is required', invalid_type_error: 'Author ID must be a string' }),
   title: z.string({ required_error: 'Title is required', invalid_type_error: 'Title must be a string' }),
   content: z.string({ required_error: 'Content is required', invalid_type_error: 'Content must be a string' }),
   viewCount: z.number({ required_error: 'View count is required', invalid_type_error: 'View count must be a number' }),
