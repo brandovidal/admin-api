822736b20fc5e18e93a9752169603db5d71fb25f
Brando Vidal <yisusdracon@gmail.com>
2023-01-28 15:42:39 -0500

    ⚒  redefine routes
    Signed-off-by: Brando Vidal <yisusdracon@gmail.com>

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -11,8 +11,8 @@ import { AppError } from '../utils/appError'
 const router = Router()
 
 router.use('/api/auth', authRouter)
-router.use('/users', userRouter)
-router.use('/posts', postRouter)
+router.use('/api/users', userRouter)
+router.use('/api/posts', postRouter)
 
 router.get('/', function (req, res) {
   res.send('Node API is running!')
@@ -30,11 +30,9 @@ router.use(
 
 router.all('*', (req: Request, res: Response, next: NextFunction): void => {
   next(AppError(404, 'not_found', `Route ${req.originalUrl} not found`))
-  // res.send('Route not found')
 })
 
 router.use((err: BaseError, req: Request, res: Response, next: NextFunction) => {
-  console.log({ err })
   const { status, code, message } = err
 
   res.status(status).json({
