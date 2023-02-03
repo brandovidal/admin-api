7b9ff49c0754fed8b56a9b9d13b6bff56b620854
Brando Vidal <yisusdracon@gmail.com>
2023-02-03 12:18:13 -0500

    ⚒  update schema and isert data
    Signed-off-by: Brando Vidal <yisusdracon@gmail.com>

--- examples/insert-data.ts
+++ examples/insert-data.ts
@@ -7,8 +7,12 @@ const prisma = new PrismaClient()
 
 export const insertUserAndPost = async (): Promise<void> => {
   await prisma.user.deleteMany()
+  await prisma.enrollment.deleteMany()
+
   await prisma.course.deleteMany()
   await prisma.program.deleteMany()
+
+  await prisma.student.deleteMany()
 
   const userInput: Prisma.UserUncheckedCreateInput = {
     email: 'ian.watson@got.com',
@@ -20,28 +24,13 @@ export const insertUserAndPost = async (): Promise<void> => {
   await prisma.user.create({
     data: userInput
   })
+  logger.info('User created')
 
   const courseInput: Prisma.CourseCreateInput ={
     name: 'Programacion',
     code: 'PROGRAMACION',
     uniqueProgram: false,
-    status: false,
-    program: {
-      createMany: {
-        data: [
-          {
-            name:  'Usando Prisma',
-            code: 'USANDOPRISMA',
-            status: true
-          },
-          {
-            name:  'Implementando Prisma',
-            code: 'IMPLEMENTANDOPRISMA',
-            status: false
-          }
-        ]
-      }
-    }
+    status: false
   }
 
   const course = await prisma.course.create({
@@ -50,16 +39,28 @@ export const insertUserAndPost = async (): Promise<void> => {
   logger.info('Course created')
   // logger.info(JSON.stringify(course, null, 2))
 
-  const program = await prisma.program.findFirst()
-  logger.info('Program created')
-  // logger.info(JSON.stringify(program, null, 2))
+  const programInput: Prisma.ProgramCreateManyInput[] = [
+    {
+      name:  'Usando Prisma',
+      code: 'USANDOPRISMA',
+      status: true,
+      courseId: course.id
+    },
+    {
+      name:  'Implementando Prisma',
+      code: 'IMPLEMENTANDOPRISMA',
+      status: false,
+      courseId: course.id
+    }
+  ]
 
-  // await prisma.program.delete({
-  //   where: {
-  //     id: program?.id
-  //   }
-  // })
-  // logger.info('Program deleted')
+  await prisma.program.createMany({
+    data: programInput
+  })
+  logger.info('Programs created')
+
+  const program = await prisma.program.findFirst({})
+  logger.info(JSON.stringify(program, null, 2))
 
   const studentInput: Prisma.StudentCreateInput = {
     name: 'Juan',
@@ -77,6 +78,37 @@ export const insertUserAndPost = async (): Promise<void> => {
   })
   logger.info(JSON.stringify(student, null, 2))
   logger.info('Student created')
+
+  const enrollemntInput: Prisma.EnrollmentCreateInput = {
+    faceToFaceModality: true,
+    marketingAds: ['recomendation', 'email'],
+    marketingEmail: 'juan@email.com',
+    marketingMedia: 'Correo electronico',
+    enableMarketingAds: true,
+    student: {
+      connect: {
+        id: student.id
+      }
+    },
+    program: {
+      connect: {
+        id: program?.id
+      }
+    }
+  }
+
+  const enrollment = await prisma.enrollment.create({
+    data: enrollemntInput
+  })
+  logger.info(JSON.stringify(enrollment, null, 2))
+  logger.info('Enrollment created')
+
+  // await prisma.student.delete({
+  //   where: {
+  //     id: student.id
+  //   }
+  // })
+  // logger.info('Student created')
 
   await prisma.$disconnect()
 }
